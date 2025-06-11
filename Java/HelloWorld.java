08140794805468255fa779202e2c016116537183
Joao-8598 <160759065+Joao-8598@users.noreply.github.com>
2025-06-11 08:29:52 -0300

    UpLoad

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+        System.out.println("Meu primeiro programa em Java!");
     }
 }
-
